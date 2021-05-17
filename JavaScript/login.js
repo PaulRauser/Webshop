630a48613d3db557d09006b6cb8102bac07cb691
PaulRauser <p.rauser@outlook.com>
2021-05-17 22:57:32 +0200

    Another Update with a lot of shit

--- JavaScript/login.js
+++ JavaScript/login.js
@@ -1,8 +1,17 @@
-const usernameInput = document.querySelectorAll('.custom-login-input');
-const passwordInput = document.querySelectorAll('.custom-login-input');
+const usernameInput = document.querySelectorAll('.custom-login-input')[0];
+const passwordInput = document.querySelectorAll('.custom-login-input')[1];
 
 const loginSubmit = document.querySelector('.custom-login-button');
+const legend = document.querySelector('.login-legend');
 
 loginSubmit.addEventListener('click', () => {
-    console.log('Hallo');
+    if(usernameInput.value.length < 3 || !usernameInput.value.includes("@")) {
+        legend.innerHTML = "Fick dich: Mindestens 3 Buchstaben und ein @, du Hurensohn.";
+    }    
+    else if(passwordInput.value.length == 0) {
+        legend.innerHTML = "Fick dich: Mindestens 1 Zeichen für Passwort";
+    }
+    else {
+        legend.innerHTML = "Login";
+    }
 });
