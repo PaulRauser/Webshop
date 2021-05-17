c84ff37153326954fb2b9c9782fb7f67f6651083
PaulRauser <p.rauser@outlook.com>
2021-05-17 21:03:51 +0200

    Added a lot of new stuff in several files. Commiting because fucking JS doesnt work

---
+++ JavaScript/login.js
@@ -0,0 +1,8 @@
+const usernameInput = document.querySelectorAll('.custom-login-input');
+const passwordInput = document.querySelectorAll('.custom-login-input');
+
+const loginSubmit = document.querySelector('.custom-login-button');
+
+loginSubmit.addEventListener('click', () => {
+    console.log('Hallo');
+});
