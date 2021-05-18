330cfe774e1fbaac62f8dc61810ccd5fb57e474f
PaulRauser <p.rauser@outlook.com>
2021-05-19 00:05:17 +0200

    New stuff

---
+++ JavaScript/product_page.js
@@ -0,0 +1,7 @@
+// const images = document.querySelector('.product-carousel');
+
+// document.querySelectorAll('.product-carousel-item').forEach(item => {
+//     item.addEventListener('click', () => {
+//         images.style.width = "100%";
+//     });
+// });
