bdbda24d71255c970726de99411a7e779d50643a
PaulRauser <p.rauser@outlook.com>
2021-05-24 00:31:36 +0200

    Neuer Raccoon

---
+++ PHP/raccoon.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Skibble</title>
+
+    <link rel="stylesheet" href="../CSS/raccoon.css">
+    <link rel="stylesheet" href="../CSS/general_frontend_styles.css">
+</head>
+<body>
+    <h1 style="color:white;  font-weight:800; font-size:40px;">Thank you for your order!</h1>
+    <a class="return-link" href="index.php">Return to Homepage</a>
+</body>
+</html>
