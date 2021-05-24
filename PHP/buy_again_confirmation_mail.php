e7d0af5a5ca400eafad77c321f64eaf08f8fc347
PaulRauser <p.rauser@outlook.com>
2021-05-24 14:28:30 +0200

    Buy again email

---
+++ PHP/buy_again_confirmation_mail.php
@@ -0,0 +1,112 @@
+<?php
+
+session_name("timlshop");
+session_start();
+
+// Array[Array[Produktname, Bilderlink(Small), Preis, Amount, description], Summe]
+include_once "PHP_Functions/shopping_cart_functions.php";
+$personalShoppingCartData = getShoppingCartData($_SESSION["email"] ?? "");
+
+$sLastName = $_SESSION['last_name'];
+$sEmail = $_SESSION['email'];
+
+
+// $totalPrice = $_POST['post-total-price']; 
+$shippingMethod = $_POST['post-shipping-method']; 
+
+$totalPrice = null;
+if($shippingMethod == "DHL") {
+    $totalPrice = $personalShoppingCartData["discounted_sum"] + 8.99;
+} elseif ($shippingMethod == "DHL-Express") {
+    $totalPrice = $personalShoppingCartData["discounted_sum"] + 23.99;
+} elseif ($shippingMethod == "FedEx") {
+    $totalPrice = $personalShoppingCartData["discounted_sum"] + 3.99;
+}
+
+$orderedProducts = "";
+
+foreach($personalShoppingCartData["pData"] as $product) {
+    $orderedProducts .= " " . $product["amount"] . "x " .  $product["name"] . ",";
+}
+
+
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require('../vendor/autoload.php');
+
+$datenbankname = "timl";
+$benutzername = "tim";
+$benutzerpasswort = "q9Xlx6Hk7Vpl";
+$servername = "chelex.life";
+
+$mail = new PHPMailer(TRUE);
+
+//Use SMTP
+$mail->isSMTP();
+
+$mail->Host = "smtp.gmail.com";
+
+$mail->Port = 587;
+
+$mail->SMTPAuth = true;
+$mail->SMTPSecure = 'tls';
+
+//Username
+$mail->Username = "skibble987@gmail.com";
+
+//Passwort --> Account Password: abcde54321! --> Das drunter ist ein App Passwort.
+//App Password for access from other application
+$mail->Password = "aiajdpnceltqnbcq";
+
+// Open try Catch
+try {
+    //Set mail sender
+    $mail->setFrom('skibble987@gmail.com', 'Skibble');
+
+    //Set recipient
+
+    $mail->addAddress($sEmail, $sLastName);
+    //Set subject
+    $mail->Subject = "Order confirmation - Skibble";
+
+    
+    //Mail Body
+    $mail->Body = "<div>" . htmlentities(" Thank you for your order. You ordered:" . $orderedProducts . " with " . $shippingMethod . " for totally " . $totalPrice . "€") . "</div>"; //Number of product/name of product, number of product... with Versandoption for Gesamtsumme (reduziert)";
+    
+    $mail->isHTML(true);
+    
+    $mail->send();
+}
+
+catch(Exception $e) {
+    //PHP Mailer Exception
+    echo $e->errorMessage();
+}
+
+catch(\Exception $e) {
+    //PHP Exception (Backslash to select the global namespace Exception class)
+    echo $e->getMessage();
+}
+
+
+$userId = getIdFromUserByEmail($sEmail);
+
+
+
+completeOrder($userId, $totalPrice, $shippingMethod);
+
+
+foreach($personalShoppingCartData["pData"] as $product) {
+    addItemToOrder(getRecentOrderIdFromUser($userId), $product["product_id"], $product["amount"], $product["discounted_price"]);
+}
+
+deleteFromShoppingCart($sEmail);
+
+
+
+
+// Mail versenden und dann an Login weitergeben
+header("Location: raccoon.php");
+?>
